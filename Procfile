610cc14cb3d632ab9144c79177db0186ae4f67c7
Soonwoong.Baek <peniel76@naver.com>
2019-02-12 12:18:23 +0900

    Heroku add

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn sabanna.wsgi --log-file -
--- sabanna/settings.py
+++ sabanna/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'u-5rph2xbb&r)#&3m5c-(_^s2@24$$m(71^!3@^m)o0rzz=dyf'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -119,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
