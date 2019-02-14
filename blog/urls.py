a1d0092895b35a68d5a2d4b45a65b5732ab027df
Soonwoong.Baek <peniel76@naver.com>
2019-02-14 13:09:25 +0900

    이미지생성함수 추가

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,6 @@
 
 from django.urls import path, register_converter
-from blog.views import index, hello_times, articles_by_year, naver_realtime_keywords, naver_blog_search
+from blog.views import index, hello_times, articles_by_year, naver_realtime_keywords, naver_blog_search, 이미지생성응답
 
 from blog.converters import FourDigitYearConverter, SlugUnicodeConverter
 
@@ -15,4 +15,5 @@ urlpatterns = [
     path('articles/<year:year>/', articles_by_year),
     path('naver/실시간검색어/', naver_realtime_keywords),
     path('naver/실시간블로그검색/', naver_blog_search),
+    path('사원증/', 이미지생성응답),
 ]
--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,9 @@ from django.http import HttpResponse
 import requests
 from bs4 import BeautifulSoup
 
+from io import BytesIO
+from PIL import Image, ImageDraw, ImageFont
+
 def index(request):
     return render(request, 'blog/index.html')
 
@@ -25,32 +28,64 @@ def naver_realtime_keywords(request):
     return HttpResponse(text)
 
 def naver_blog_search(request):
-    query = request.GET.get('query')
+    query = request.GET.get('query', '')
+    post_list=[]
 
-    url = 'https://search.naver.com/search.naver'
+    if query:
+        url = 'https://search.naver.com/search.naver'
 
-    params = {
-        'where': 'post',
-        'sm': 'tab_jum',
-        'query': query,
-    }
+        params = {
+            'where': 'post',
+            'sm': 'tab_jum',
+            'query': query,
+        }
 
-    res = requests.get(url, params=params)
-    html = res.text
-    soup = BeautifulSoup(html, 'html.parser')
+        res = requests.get(url, params=params)
+        html = res.text
+        soup = BeautifulSoup(html, 'html.parser')
 
-    tag_list = soup.select('#elThumbnailResultArea .sh_blog_title')
+        tag_list = soup.select('#elThumbnailResultArea .sh_blog_title')
 
-    post_list = []
-    for tag in tag_list:
-        post_title = tag.text
-        post_url = tag['href']
-        post_list.append({
-            'title': post_title,
-            'url': post_url,
-        })
+        post_list = []
+        for tag in tag_list:
+            post_title = tag.text
+            post_url = tag['href']
+            post_list.append({
+                'title': post_title,
+                'url': post_url,
+            })
 
     return render(request, 'blog/naver_blog_search.html', {
         'query': query,
         'post_list': post_list,
     })
+
+def 이미지생성응답(request):
+    #ttf_path = 'C:/Windows/Fonts/malgun.ttf' # 윈도우, 맥:
+    ttf_path = 'assets/Fonts/AppleGothic.ttf'
+
+    image_url = 'http://www.flowermeaning.com/flower-pics/Calla-Lily-Meaning.jpg'
+    #image_url = 'assets/images/s1.jpg'
+
+    res = requests.get(image_url) # 서버로 HTTP GET 요청하여, 응답 획득
+
+    io = BytesIO(res.content) # 응답의 Raw Body  메모리 파일 객체 BytesIO 인스턴스 생성
+    io.seek(0) # 파일의 처음으로 커서를 이동
+    canvas = Image.open(io).convert('RGBA') # 이미지 파일을 열고, RGBA 모드로 변환
+    font = ImageFont.truetype(ttf_path, 15) # 지정 경로의 TrueType 폰트, 폰트크기 40
+    draw = ImageDraw.Draw(canvas) # canvas에 대한 ImageDraw 객체 획득
+
+    text = request.GET.get('name', '익명')
+    left, top = 10, 10
+    margin = 10
+    width, height = font.getsize(text)
+    right = left + width + margin
+    bottom = top + height + margin
+
+    draw.rectangle((left, top, right, bottom), (255, 255, 224))
+    draw.text((15,15), text, font=font, fill=(20, 20, 20))
+
+    response = HttpResponse(content_type='image/png')
+    canvas.save(response, format='PNG')
+
+    return response
