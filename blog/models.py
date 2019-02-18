e234348eb0ab7f6c40c79f94bac1a07474f90dbd
BAEK SOON WOONG <peniel76@naver.com>
2019-02-18 09:30:43 +0900

    comment table create

--- blog/models.py
+++ blog/models.py
@@ -6,3 +6,9 @@ class Post(models.Model):
     contents=models.TextField()
     create_at=models.DateTimeField(auto_now_add=True)
     update_at=models.DateTimeField(auto_now=True)
+
+class Comment(models.Model):
+    author_name=models.CharField(max_length=100)
+    message=models.TextField()
+    create_at=models.DateTimeField(auto_now_add=True)
+    update_at=models.DateTimeField(auto_now=True)
