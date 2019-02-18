0e32927b6d7b54d72afac1111b399ab56924875d
BAEK SOON WOONG <peniel76@naver.com>
2019-02-18 12:06:27 +0900

    post author field added

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,13 @@
 from django.db import models
+#from django.contrib.auth.models import User
+from django.conf import settings  # 설정값을 가져오는 올바른 방법
+
+# settings --> auth
+# django/django/conf/global_settings.py  --> auth.User 활용
 
 # Create your models here.
 class Post(models.Model):
+    author=models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title=models.CharField(max_length=100, db_index=True)
     contents=models.TextField()
     tags=models.CharField(max_length=20)
@@ -13,6 +19,8 @@ class Post(models.Model):
         return self.title
 
 class Comment(models.Model):
+    author=models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    ost=models.ForeignKey(Post, on_delete=models.CASCADE)
     author_name=models.CharField(max_length=100)
     message=models.TextField()
     create_at=models.DateTimeField(auto_now_add=True)
