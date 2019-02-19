a76529171414bc348a65bfec05162acdaf5ceffd
BAEK SOON WOONG <peniel76@naver.com>
2019-02-19 09:22:26 +0900

    shop field 수정

--- shop/models.py
+++ shop/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Shop(models.Model):
     name=models.CharField(max_length=100)
     desc=models.TextField(blank=True)
-    address=models.CharField(max_length=50)
+    address=models.CharField(max_length=50, blank=True)
     create_at=models.DateTimeField(auto_now_add=True)
     update_at=models.DateTimeField(auto_now=True)
 
@@ -15,6 +15,10 @@ class Item(models.Model):
     shop=models.ForeignKey(Shop, on_delete=models.CASCADE)
     name=models.CharField(max_length=100)
     desc=models.TextField(blank=True)
+    price=models.PositiveIntegerField()
     is_public=models.BooleanField(default=False)
     create_at=models.DateTimeField(auto_now_add=True)
     update_at=models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
